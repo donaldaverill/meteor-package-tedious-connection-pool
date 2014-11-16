b025cce3a6aa29db72bb744b8d78ffd29628abea
Donald Averill <donaldaverill@gmail.com>
2014-11-16 15:16:17 -0800

    0.3.0_2

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'donaldaverill:tedious-connection-pool',
   summary: 'tedious-connection-pool npm package wrapped for Meteor',
-  version: '0.3.0_1',
+  version: '0.3.0_2',
   git: 'https://github.com/donaldaverill/meteor-package-tedious-connection-pool'
 });
 Npm.depends({
@@ -10,7 +10,7 @@ Npm.depends({
 });
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.use('donaldaverill:tedious', ['server']);
+  api.use('donaldaverill:tedious@1.7.0_2', ['server']);
   if (typeof Tedious === 'undefined') {
     api.export('Tedious', ['server']);
   }
